4d935040b914b1d146226f942eed9cfc2220dd44
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-06 16:23:57 -0700

    Update sizing for map display.

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,3 +1,4 @@
+// Render map
 const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=VOUVJ6EuCCKH8cA2JsJb', {
   attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
