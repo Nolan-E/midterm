2ac5bd6f550a75bc83e8f36738064371c55a31fa
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-06 14:49:14 -0700

    Update index template to show map. Post-rebase 3

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,19 +1,4 @@
-// Center of London map focus
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(mymap);
+const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=VOUVJ6EuCCKH8cA2JsJb', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    // maxZoom: 18,
-    // id: 'mapbox/streets-v11',
-    // tileSize: 512,
-    // zoomOffset: -1,
-    // accessToken: 'your.mapbox.access.token'
+  attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 }).addTo(mymap);
--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/test/", (req, res) => {
+  res.render("test");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
