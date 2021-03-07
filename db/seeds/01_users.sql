a9e769f8b82bfd2a8dd92cea5333929bd3bfa43e
ermurray <ermurray31@gmail.com>
2021-03-07 08:42:03 +0000

    adjusted database files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@wonderland.com', 'plainfornow'), ('Kira', 'kira@knight.com', 'password'), ('Batman', 'bruce@waynemanor.com', 'iambatman'), ('Guy', 'that@guy.com', 'thatguy'), ('test','test@testy.com', '1234'), ('Demo', 'demo@example.com', 'demoday');
