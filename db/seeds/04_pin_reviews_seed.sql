b59fd1944d8976ec1d4080041c6a0f6195d5e8f7
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-08 14:39:48 -0700

    Create pin reviews seed.

---
+++ db/seeds/04_pin_reviews_seed.sql
@@ -0,0 +1,8 @@
+-- Pin_reviews table seeds here
+INSERT INTO pin_reviews (pin_id, user_id, stars, message, date_created)
+VALUES (1, 3, 3, 'Calgary', '1989-03-25T15:30:00.000Z'),
+  (2, 4, 4, 'Decent!', '1997-12-11T01:21:00.000Z'),
+  (3, 6, 4, 'Well...', '2000-01-04T08:10:00.000Z'),
+  (4, 6, 5, 'Not bad.', '2003-02-27T19:31:00.000Z'),
+  (5, 6, 2, 'Wild!', '2005-04-13T11:43:00.000Z'),
+  (6, 2, 1, 'Will be back!', '2020-02-15T07:08:52.000Z');
