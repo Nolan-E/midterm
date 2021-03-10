7bf49d2bd0f29d66e02feade1c85dc3b75d402a3
vagrant <gio_wan@live.ca>
2021-03-10 07:02:33 +0000

    make map name clickable to get the map's map_id

--- public/scripts/nav-favorite-maps.js
+++ public/scripts/nav-favorite-maps.js
@@ -6,10 +6,23 @@ const showFavoriteMaps = () => {
       $("#map-info-area").append("<h1>Favorite Maps</h1>");
       for (const map of maps) {
         const createMapCard = `
-        <div class="card border-primary mb-2">
+        <div class="card border-primary">
+
           <div class="card-header">${map.created_by}</div>
           <div class="card-body text-primary">
-            <h5 class="card-title">${map.map_name}</h5>
+            <form class="form-map-name">
+              <input type="hidden" name="map_id" value="${map.map_id}">
+              <button class="button-map-name" type="submit">${map.map_name}</button>
+              <h5 class="card-title">
+                <a href="" type="submit">
+                ${map.map_name}
+                </a>
+              </h5>
+            </form>
+
+
+
+
             <small id="map-author-rating">
               <p class="card-text">Rating: ${map.rating}/5</p>
               <p class="card-text">${map.map_created}</p>
@@ -25,5 +38,11 @@ const showFavoriteMaps = () => {
 
 $(document).ready(function() {
   $("#nav-favorite-maps").on("click", showFavoriteMaps);
+
+  $(document).on("submit", ".form-map-name", function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    console.log(data);
+  })
 
 });
