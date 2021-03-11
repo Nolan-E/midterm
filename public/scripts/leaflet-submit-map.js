5920574b5c5ead513f6a61dd6a4aec9195bcce36
ermurray <ermurray31@gmail.com>
2021-03-11 09:50:27 +0000

    still not able to remove div element based on marker pin

--- public/scripts/leaflet-submit-map.js
+++ public/scripts/leaflet-submit-map.js
@@ -8,7 +8,7 @@ markerGroup.addTo(mymap);
 let editPopup = L.popup({
   editable: true,
   removable:true,
-  nametag: `newPin${1}`
+  nametag: `${i}`
 
 }).setContent('delBtn', 'place holder for custom html elment');
 
@@ -19,23 +19,23 @@ const onMapClickAddMarker = function(e) {
 
   Arr.push([lat, lng]);
   // console.log('Arr is now', Arr);
-  markerGroup.addLayer(marker = L.marker([lat, lng]).addTo(mymap)
-    .bindPopup(editPopup)
+  markerGroup.addLayer(new L.marker([lat, lng]).addTo(mymap)
+    .bindPopup(editPopup).update()
 
   );
   const appendField = `
-      <div class="card border-dark mt-3 mb-1 create-card" id="pin-${i}">
+      <div class="card border-dark mt-3 mb-1 create-card" id="pin${lat}">
         <div>
           <label for="pin-title">Name:</label>
           <input type="text" name="pins[${i}][pin-title]" required>
         </div>
         <div>
           <label for="pin-latitude">Latitude:</label>
-          <input type="text" name="pins[${i}][pin-latitude]" value=${lat} readonly>
+          <input type="text" name="pins[${i}][pin-latitude]" value=${lat} hidden>
         </div>
         <div>
           <label for="pin-longitude">Longitude:</label>
-          <input type="text" name="pins[${i}][pin-longitude]" value=${lng} readonly>
+          <input type="text" name="pins[${i}][pin-longitude]" value=${lng} hidden>
         </div>
         <div>
           <label for="pin-description">Description:</label>
@@ -47,15 +47,21 @@ const onMapClickAddMarker = function(e) {
   $("#form-container").append(appendField);
   $("#form-container").append("<br>");
   i ++;
-  $(document).on("removeMarker", function() {
-    console.log('i heard the remove event');
-  });
   // console.log('inside', i);
 
 };
 
 mymap.on('click', onMapClickAddMarker);
-
+$(document).on("removeMarker", function(e) {
+  let lat = e.detail.marker._latlng.lat;
+  let lng = e.detail.marker._latlng.lng;
+  let mapCard = `pin${lat}`;
+  $("#form-container").contents().remove("#" + mapCard);
+  console.log(mapCard);
+});
 document.addEventListener("removeMarker", (e) => {
-  console.log(e);
+
+
+  console.log(e.detail.marker._latlng.lng);
+  console.log(e.detail.marker._latlng.lat);
 });
