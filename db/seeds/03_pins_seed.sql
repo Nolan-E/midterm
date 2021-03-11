f86e0f4ad609e4593d48d90db544c11c508ca1a9
vagrant <gio_wan@live.ca>
2021-03-11 18:18:50 +0000

    added two more pins for s-toon lagoon

--- db/seeds/03_pins_seed.sql
+++ db/seeds/03_pins_seed.sql
@@ -12,4 +12,8 @@ VALUES (1, 51.0447, -114.0719, 'Calgary', 'Location of Calgary', default),
   (4, 49.8880, -119.4960, 'Kelowna', 'Location of Kelowna', default),
   (5, 44.6488, -63.5752, 'Halifax', 'Location of Halifax', default),
   (6, 52.1579, -106.6702, 'Saskatoon', 'Location of Saskatoon', default),
-  (1, 51.0447, -114.0719, 'Calgary', 'Location of Calgary', default);
+  (1, 51.0447, -114.0719, 'Calgary', 'Location of Calgary', default),
+  (6, 52.1578, -106.6701, 'Saskatoon2', 'Location of Saskatoon2', default),
+  (6, 52.1580, -106.6703, 'Saskatoon3', 'Location of Saskatoon3', default);
+
+
