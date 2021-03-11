31bb129e02952c48c7050c093fa2532a25a7c64d
ermurray <ermurray31@gmail.com>
2021-03-11 03:22:42 +0000

    differentiate between create card class and others on click

--- public/scripts/map-render-pins.js
+++ public/scripts/map-render-pins.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $(document).on('click', '.card', function() {
+  $(document).on('click', '.card:not(.create-card)', function() {
       let id = this.id
       console.log('clicked this:',id);
-      markerGroup.clearLayers();
-    $.get(`/api/maps/${id}`)
-    .then(maps => {
+      $.get(`/api/maps/${id}`)
+      .then(maps => {
+        markerGroup.clearLayers();
         for(const pin of maps){
           markerGroup.addLayer(marker = L.marker([pin.pin_lat, pin.pin_lng]).addTo(mymap)
           .bindPopup(editPopup));
