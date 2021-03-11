98c6ecd32ee2359a5f744165c23f70fb2cca3cca
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-11 13:04:58 -0700

    Unbroke some queries. Update jQuery for explore maps & my maps side panel.

--- public/scripts/nav-explore-maps.js
+++ public/scripts/nav-explore-maps.js
@@ -1,18 +1,24 @@
 const showExploreMaps = () => {
-  $("#map-info-area").empty();
-  $.get("api/maps")
-    .then(maps => {
-      console.log(maps);
-      $("#map-info-area").append("<h1>Explore Maps</h1>");
-      for (const map of maps) {
-        const createMapCard = `
+  $("#map-info-area").empty()
+    $.get("api/maps")
+      .then(maps => {
+        console.log(maps)
+        $("#map-info-area").append("<h1>Explore Maps</h1>");
+        for (const map of maps) {
+          let ratingStr = '';
+          if (map.rating) {
+            ratingStr = `Rating: ${map.rating}`;
+          } else {
+            ratingStr = `No rating`
+          }
+          const createMapCard = `
           <div class="card border-primary mb-2" id=${map.map_id}>
             <img src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80" class= "card-img-top">
             <div class="card-header">By ${map.created_by}</div>
             <div class="card-body text-primary">
               <h5 class="card-title">${map.map_name}</h5>
               <small id="map-author-rating">
-                <p class="card-text">Rating: ${map.rating}/5</p>
+                <p class="card-text">${ratingStr}</p>
                 <p class="card-text">${map.map_created}</p>
               </small>
               <form class="add-to-favorites">
@@ -40,7 +46,6 @@ $(document).ready(function() {
     const mapId = Number($(this).serializeArray()[0].value);
     $.post("api/maps/addtofavorites", {mapId})
       .then((data) => {
-<<<<<<< 913c44efc0f1e718bdbd1bf9f925ba741933b012
         console.log("Added to favorites!");
         console.log('The following entry has been added into the fav_maps table: ', data)
       })
@@ -49,10 +54,3 @@ $(document).ready(function() {
       });
   })
 })
-=======
-        console.log('The following entry has been added into the fav_maps table: ', data);
-      })
-      .catch(error => console.log(error));
-  });
-});
->>>>>>> add popup detail view for existing map pins.
