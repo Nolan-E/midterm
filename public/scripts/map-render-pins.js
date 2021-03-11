199a767aea2d22f397497fd58e0b9e975bb25fbb
vagrant <gio_wan@live.ca>
2021-03-11 17:58:21 +0000

    tried to zoom in on pin when user clicks on the pin card

--- public/scripts/map-render-pins.js
+++ public/scripts/map-render-pins.js
@@ -17,28 +17,24 @@ $(document).ready(function() {
       });
     });
 
-    // Centers the view on a single pin when a pin-card is clicked.
-    $(document).on('click', '.pin-card', function() {
-      let id = Number(this.id);
-      $.get(`/api/pins/${id}`)
-      .then(pins => {
-        // markerGroup.clearLayers();
-        for(const pin of pins){
-          if (pin.pin_id === id) {
-            console.log('check passed')
-            console.log('Now need to figure out how to center map on this pin. map-render-pins file')
-            // mymap.setView([pin.pin_lng, pin.pin_lat])
-            // L.map('mapid').setView([pin.pin_lng, pin.pin_lat])
-            // markerGroup.setView([pin.pin_lat, pin.pin_lng], 13)
-          }
+  // Centers the view on a single pin when a pin-card is clicked.
+  $(document).on('click', '.pin-card', function() {
+    let id = Number(this.id);
+    $.get(`/api/pins/${id}`)
+    .then(pins => {
+      // markerGroup.clearLayers();
+      console.log('the pins returned are: ', pins)
+      for(const pin of pins){
+        if (pin.pin_id === id) {
+          console.log('check passed')
+          // console.log('this pins details are', pin)
 
-          // markerGroup.addLayer(marker = L.marker([pin.pin_lat, pin.pin_lng]).addTo(mymap)
-          // .bindPopup(editPopup));
+          markerGroup.addLayer(marker = L.marker([pin.pin_lat, pin.pin_lng]).addTo(mymap)
+          .bindPopup(editPopup));
+          markerGroup.addTo(mymap);
+          mymap.fitBounds(markerGroup.getBounds());
         }
-        // markerGroup.addTo(mymap);
-        // mymap.fitBounds(markerGroup.getBounds());
-        // console.log('redender these:',maps)
-      });
+      }
     });
-
+  });
 });
