54cd447c41136df91c48f1e90b307b265eaa26fc
vagrant <gio_wan@live.ca>
2021-03-11 21:06:04 +0000

    added map-card class to explore maps

--- public/scripts/nav-explore-maps.js
+++ public/scripts/nav-explore-maps.js
@@ -12,7 +12,7 @@ const showExploreMaps = () => {
             ratingStr = `No rating`
           }
           const createMapCard = `
-          <div class="card border-primary mb-2" id=${map.map_id}>
+          <div class="card border-primary mb-2 map-card" id=${map.map_id}>
             <img src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80" class= "card-img-top">
             <div class="card-header">By ${map.created_by}</div>
             <div class="card-body text-primary">
