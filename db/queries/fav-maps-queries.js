c60943f9c8ccf586e71abdb7959e1b1ffd01f57f
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-10 20:00:23 -0700

    Add deleteMyFav query.

--- db/queries/fav-maps-queries.js
+++ db/queries/fav-maps-queries.js
@@ -9,7 +9,7 @@ const getAllFavMaps = (userID) => {
   JOIN maps ON fav_maps.map_id = maps.id
   JOIN pins ON pins.map_id = maps.id
   JOIN users ON maps.user_id = users.id
-  WHERE fav_maps.user_id = $1
+  WHERE fav_maps.user_id = $1 AND maps.active = true AND fav_maps.active = true
   GROUP BY maps.id, users.name, pins.image_url, fav_maps.map_id, fav_maps.rating, fav_maps.review, fav_maps.fav_date
   ORDER BY fav_date DESC;`, [userID]
   )
@@ -25,12 +25,22 @@ const addToMyFav = (userID, mapID) => {
     .then(response => response.rows[0])
     .catch(err => err);
 };
+// Remove a map from my fav maps
+const deleteMyFav = (userID, mapID) => {
+  return db.query(`
+    UPDATE fav_maps
+    SET active = false
+    WHERE map_id = $2 AND user_id = $1 RETURNING *;`, [userID, mapID]
+  )
+    .then(response => response.rows[0])
+    .catch(err => err);
+};
 // Add a rating & review to a fav map by user
 const addFavReviewRating = (userID, mapID, rating, review) => {
   return db.query(`
     UPDATE fav_maps
     SET rating = $3, review = $4, last_edit = current_timestamp
-    WHERE user_id = $1 and map_id = $2 RETURNING *;`, [userID, mapID, rating, review] // add current_timestamp to table
+    WHERE user_id = $1 and map_id = $2 RETURNING *;`, [userID, mapID, rating, review]
   )
     .then(response => response.rows[0])
     .catch(err => err);
@@ -40,5 +50,6 @@ const addFavReviewRating = (userID, mapID, rating, review) => {
 module.exports = {
   getAllFavMaps,
   addToMyFav,
+  deleteMyFav,
   addFavReviewRating
 };
