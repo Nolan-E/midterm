d1c55e090a1a50b7a853c94da2edbf9754fe07f9
ermurray <ermurray31@gmail.com>
2021-03-12 06:00:33 +0000

    linting and styled button for favorite on explore maps. installed bootstrap icons

--- public/scripts/nav-explore-maps.js
+++ public/scripts/nav-explore-maps.js
@@ -1,17 +1,16 @@
 const showExploreMaps = () => {
-  $("#map-info-area").empty()
-    $.get("api/maps")
-      .then(maps => {
-        console.log(maps)
-        $("#map-info-area").append("<h1>Explore Maps</h1>");
-        for (const map of maps) {
-          let ratingStr = '';
-          if (map.rating) {
-            ratingStr = `Rating: ${map.rating}`;
-          } else {
-            ratingStr = `No rating`
-          }
-          const createMapCard = `
+  $("#map-info-area").empty();
+  $.get("api/maps")
+    .then(maps => {
+      $("#map-info-area").append("<h1>Explore Maps</h1>");
+      for (const map of maps) {
+        let ratingStr = '';
+        if (map.rating) {
+          ratingStr = `Rating: ${map.rating}`;
+        } else {
+          ratingStr = `No rating`;
+        }
+        const createMapCard = `
           <div class="card border-primary mb-2 map-card" id=${map.map_id}>
             <img src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80" class= "card-img-top">
             <div class="card-header">By ${map.created_by}</div>
@@ -23,9 +22,7 @@ const showExploreMaps = () => {
               </small>
               <form class="add-to-favorites">
                 <input type="hidden" name="map_id" value="${map.map_id}">
-                <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-              </svg></button>
+                <button type="submit" class="btn btn-outline-primary"><i class="bi bi-suit-heart"></i></button>
               </form>
 
             </div>
@@ -49,10 +46,10 @@ $(document).ready(function() {
     $.post("api/maps/addtofavorites", {mapId})
       .then((data) => {
         console.log("Added to favorites!");
-        console.log('The following entry has been added into the fav_maps table: ', data)
+        console.log('The following entry has been added into the fav_maps table: ', data);
       })
       .catch(error => {
         alert(error.responseText);
       });
-  })
-})
+  });
+});
