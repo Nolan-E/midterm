ce000e3e904c636480bd0ba97fe06f467351792d
ermurray <ermurray31@gmail.com>
2021-03-12 13:48:36 +0000

    fixed margin on favorite cards. added image urls to db seeds

--- public/scripts/nav-my-maps.js
+++ public/scripts/nav-my-maps.js
@@ -58,3 +58,5 @@ $(document).ready(function() {
       .catch(error => console.log(error));
   });
 });
+
+// https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80
