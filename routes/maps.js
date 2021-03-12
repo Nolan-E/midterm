c5fb8d064f959dbb8185e34b1997865cc95b8272
vagrant <gio_wan@live.ca>
2021-03-12 03:47:20 +0000

    removing comment

--- routes/maps.js
+++ routes/maps.js
@@ -22,68 +22,6 @@ const {createPin,
 const {deleteMyFav} = require('../db/queries/fav-maps-queries');
 const {isLoggedIn} = require('../public/scripts/middleware');
 
-//dumy data
-// const dataPoints = {
-//   "type": "FeatureCollection",
-//   "name": "test-points-short-named",
-//   "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-//   "features": [
-//     { "type": "Feature",
-//       "properties": {
-//         "name": "Dog Park A",
-//         "description": "AAA"
-//       },
-//       "geometry": {
-//         "type": "Point",
-//         "coordinates": [ -114.19687271118165, 51.15049396880196 ]
-//       } },
-//     { "type": "Feature",
-//       "properties": {
-//         "name": "Dog Park B",
-//         "description": "BBB"
-//       },
-//       "geometry": {
-//         "type": "Point",
-//         "coordinates": [ -114.17850494384767, 51.147801909622714 ]
-//       } },
-//     { "type": "Feature",
-//       "properties": {
-//         "name": "Dog Park C", "description": "CCC"
-//       },
-//       "geometry":
-//       { "type": "Point",
-//         "coordinates": [ -114.17678833007814, 51.13649354621719 ]
-//       } },
-//     { "type": "Feature",
-//       "properties": {
-//         "name": "Dog Park D",
-//         "description": "DDD"
-//       },
-//       "geometry": {
-//         "type": "Point", "coordinates": [ -114.19292449951173, 51.12895309822599 ]
-//       } }
-//   ]
-// };
-// const maps = {
-//   1:{
-//     mapName: 'jimbobs map of furry playGrounds',
-//     user: "jimmy",
-//     stars: 0,
-//   },
-//   2: {
-//     mapName: 'Suzy Q and her pups',
-//     user: 'NotSuzy',
-//     stars: 5
-//   },
-//   3: {
-//     mapName: 'dog park of pleasures',
-//     user: 'John Doe',
-//     stars: 2.5
-//   }
-
-
-// };
-
 router.get("/", (req, res) => {
   getAllMapsAnon()
     .then(data => {
@@ -136,9 +74,7 @@ router.get("/:id", (req,res) => {
     //   })
 });
 
-
 //maps POST Routes
-
 router.post('/', (req, res) => {
   console.log(req.body.map);
   const user_id = req.session.user_id;
