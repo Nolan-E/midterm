fdc89badd38a4c2984503e62e911b4d2968bf4ce
vagrant <gio_wan@live.ca>
2021-03-12 04:35:46 +0000

    add a button to maps in explore maps so that more information about that map will show up when clicked

--- public/scripts/nav-explore-maps.js
+++ public/scripts/nav-explore-maps.js
@@ -18,6 +18,10 @@ const showExploreMaps = () => {
             <div class="card-header">By ${map.created_by}</div>
             <div class="card-body text-primary">
               <h5 class="card-title">${map.map_name}</h5>
+              <form class="form-map-name">
+                <input type="hidden" name="map_id" value="${map.map_id}">
+                <button type="submit">${map.map_name} (Click to see more details)</button>
+              </form>
               <small id="map-author-rating">
                 <p class="card-text">${ratingStr}</p>
                 <p class="card-text">${map.map_created}</p>
